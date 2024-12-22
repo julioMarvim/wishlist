158de3e5aa55f47c4ca95950f859e13a4f10ac72
julioMarvim <juliomarvim@yahoo.com.br>
2024-12-22 17:55:09 -0300

    test: add unit test for remove method in wishlist repository

--- app/src/test/java/com/marvim/wishlist/adapter/repository/mongo/WishlistRepositoryImplTest.java
+++ app/src/test/java/com/marvim/wishlist/adapter/repository/mongo/WishlistRepositoryImplTest.java
@@ -2,6 +2,7 @@ package com.marvim.wishlist.adapter.repository.mongo;
 
 import com.marvim.wishlist.domain.entity.Product;
 import com.marvim.wishlist.domain.entity.Wishlist;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -12,31 +13,60 @@ import java.util.List;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class WishlistRepositoryImplTest {
+
     @Mock
     private SpringDataWishlistRepository springDataRepository;
 
-    @Test
-    void shouldAddProductToWishlist() {
-        String clientId = "client-id";
-        Product product = Product.builder()
+    private WishlistRepositoryImpl wishlistRepository;
+
+    private Wishlist wishlist;
+    private Product product;
+    private final String clientId = "client-id";
+
+    @BeforeEach
+    void setUp() {
+        product = Product.builder()
                 .id("1")
                 .name("Garrafa")
                 .description("Garrafa de café")
                 .price(30d)
                 .build();
 
-        Wishlist wishlist = Wishlist.builder()
+        wishlist = Wishlist.builder()
                 .clientId(clientId)
                 .products(List.of(product))
                 .build();
 
-        springDataRepository.save(wishlist);
+        wishlistRepository = new WishlistRepositoryImpl(springDataRepository);
+    }
+
+    @Test
+    void shouldAddProductToWishlist() {
+        when(springDataRepository.save(wishlist)).thenReturn(wishlist);
+
+        wishlistRepository.add(product);
 
         ArgumentCaptor<Wishlist> wishlistCaptor = ArgumentCaptor.forClass(Wishlist.class);
         verify(springDataRepository, times(1)).save(wishlistCaptor.capture());
+
+        List<Product> capturedProducts = wishlistCaptor.getValue().getProducts();
+        assert capturedProducts.contains(product);
     }
 
+    @Test
+    void shouldRemoveProductFromWishlist() {
+        when(springDataRepository.findByClientId(clientId)).thenReturn(java.util.Optional.of(wishlist));
+
+        wishlistRepository.remove(clientId, product.getId());
+
+        ArgumentCaptor<Wishlist> wishlistCaptor = ArgumentCaptor.forClass(Wishlist.class);
+        verify(springDataRepository, times(1)).save(wishlistCaptor.capture());
+
+        List<Product> capturedProducts = wishlistCaptor.getValue().getProducts();
+        assert !capturedProducts.contains(product);
+    }
 }
