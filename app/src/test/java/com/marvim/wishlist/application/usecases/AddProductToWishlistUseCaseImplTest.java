6c57a62f30fa0aecd45f5dbb6bd46e35bc62607b
julioMarvim <juliomarvim@yahoo.com.br>
2024-12-22 13:26:36 -0300

    test: add unit test for use case to verify repository save method call

---
+++ app/src/test/java/com/marvim/wishlist/application/usecases/AddProductToWishlistUseCaseImplTest.java
@@ -0,0 +1,37 @@
+package com.marvim.wishlist.application.usecases;
+
+import com.marvim.wishlist.domain.entity.Product;
+import com.marvim.wishlist.domain.ports.output.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class AddProductToWishlistUseCaseImplTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private AddProductToWishlistUseCaseImpl addProductToWishlistUseCase;
+
+    @Test
+    void shouldSaveProductWhenExecutingUseCase() {
+        Product product = Product.builder()
+                .id("1")
+                .clientId("1")
+                .name("Garrafa")
+                .description("Garrafa de café")
+                .price(30d)
+                .build();
+
+        addProductToWishlistUseCase.execute(product);
+
+        verify(productRepository, Mockito.times(1)).save(product);
+    }
+}
