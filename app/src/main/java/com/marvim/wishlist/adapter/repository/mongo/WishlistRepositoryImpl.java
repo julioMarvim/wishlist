c0ac58fe34597d1ecc1219651822027fc7717d27
julioMarvim <juliomarvim@yahoo.com.br>
2024-12-22 18:56:12 -0300

    feat: add method to get wishlist from MongoDB in repository

--- app/src/main/java/com/marvim/wishlist/adapter/repository/mongo/WishlistRepositoryImpl.java
+++ app/src/main/java/com/marvim/wishlist/adapter/repository/mongo/WishlistRepositoryImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -43,6 +44,7 @@ public class WishlistRepositoryImpl implements WishlistRepository {
 
     @Override
     public Wishlist findByClientId(String clientId) {
-        return null;
+        Optional<Wishlist> wishlist = repository.findByClientId(clientId);
+        return wishlist.orElse(null);
     }
 }
