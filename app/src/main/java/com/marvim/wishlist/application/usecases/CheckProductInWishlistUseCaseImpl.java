398d0f47ad98ed47fde38be48b78b0498dcc5be6
julioMarvim <juliomarvim@yahoo.com.br>
2024-12-24 15:41:41 -0300

    refactor: refactor method to check if product already exists in client's wishlist

--- app/src/main/java/com/marvim/wishlist/application/usecases/CheckProductInWishlistUseCaseImpl.java
+++ app/src/main/java/com/marvim/wishlist/application/usecases/CheckProductInWishlistUseCaseImpl.java
@@ -1,5 +1,7 @@
 package com.marvim.wishlist.application.usecases;
 
+import com.marvim.wishlist.config.handler.exception.ProductNotFoundException;
+import com.marvim.wishlist.config.handler.exception.WishlistNotFoundException;
 import com.marvim.wishlist.domain.entity.Wishlist;
 import com.marvim.wishlist.domain.ports.input.CheckProductInWishlistUseCase;
 import com.marvim.wishlist.domain.ports.output.WishlistRepository;
@@ -17,20 +19,18 @@ public class CheckProductInWishlistUseCaseImpl implements CheckProductInWishlist
     private final WishlistRepository wishlistRepository;
 
     @Override
-    public boolean execute(String clientId, String productId) {
+    public void execute(String clientId, String productId) {
         logger.info("Starting operation to check if product with ID: {} exists in client {}'s wishlist", productId, clientId);
 
-        boolean exists = wishlistRepository.findByClientId(clientId)
-                .map(wishlist -> wishlist.getProducts().stream()
-                        .anyMatch(product -> product.getId().equals(productId)))
-                .orElse(false);
+        Wishlist wishlist = wishlistRepository.findByClientId(clientId)
+                .orElseThrow(() -> {
+                    logger.error("Wishlist for client with ID: {} not found", clientId);
+                    return new WishlistNotFoundException(clientId);
+                });
 
-        if (exists) {
-            logger.info("Product with ID: {} exists in client {}'s wishlist", productId, clientId);
-        } else {
-            logger.info("Product with ID: {} does not exist in client {}'s wishlist", productId, clientId);
+        if (wishlist.getProducts().stream().noneMatch(product -> product.getId().equals(productId))) {
+            logger.error("Product with ID: {} not found in wishlist for client with ID: {}", productId, clientId);
+            throw new ProductNotFoundException(clientId, productId);
         }
-
-        return exists;
     }
 }
