be87ca6116909db5c99370e20152c794d5190eaa
julioMarvim <juliomarvim@yahoo.com.br>
2024-12-25 15:36:05 -0300

    test: create unit tests for wishlist validation class

---
+++ app/src/test/java/com/marvim/wishlist/domain/service/WishlistValidationServiceTest.java
@@ -0,0 +1,87 @@
+package com.marvim.wishlist.domain.service;
+
+import com.marvim.wishlist.config.handler.exception.ProductAlreadyInWishlistException;
+import com.marvim.wishlist.config.handler.exception.WishlistLimitExceededException;
+import com.marvim.wishlist.domain.entity.Product;
+import com.marvim.wishlist.domain.entity.Wishlist;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class WishlistValidationServiceTest {
+
+    @Mock
+    private Wishlist wishlist;
+
+    @InjectMocks
+    private WishlistValidationService wishlistValidationService;
+
+    @Test
+    void shouldThrowExceptionIfWishlistExceedsMaximumLimit() {
+        String clientId = "client-id";
+        Wishlist fullWishlist = Wishlist.builder()
+                .clientId(clientId)
+                .products(IntStream.range(0, 20)
+                        .mapToObj(i -> Product.builder()
+                                .id("product-" + i)
+                                .name("Produto " + i)
+                                .description("Descrição " + i)
+                                .build())
+                        .collect(Collectors.toList()))
+                .build();
+
+        WishlistLimitExceededException exception = assertThrows(WishlistLimitExceededException.class, () ->
+                wishlistValidationService.validateWishlistLimit(fullWishlist)
+        );
+
+        assertEquals("Customer ID client-id has exceeded the maximum number of products in the wishlist.", exception.getMessage());
+    }
+
+    @Test
+    void shouldNotThrowExceptionIfWishlistIsUnderLimit() {
+        Wishlist wishlist = Wishlist.builder()
+                .clientId("client-id")
+                .products(new ArrayList<>(List.of(Product.builder().id("product-1").build())))
+                .build();
+
+        assertDoesNotThrow(() -> wishlistValidationService.validateWishlistLimit(wishlist));
+    }
+
+    @Test
+    void shouldThrowExceptionIfProductAlreadyInWishlist() {
+        String clientId = "client-id";
+        Product product = Product.builder().id("product-id").build();
+        Wishlist wishlist = Wishlist.builder()
+                .clientId(clientId)
+                .products(new ArrayList<>(List.of(product)))
+                .build();
+
+        ProductAlreadyInWishlistException exception = assertThrows(ProductAlreadyInWishlistException.class, () ->
+                wishlistValidationService.validateProductNotInWishlist(wishlist, product)
+        );
+
+        assertEquals("Product with ID product-id is already in the customer id client-id wishlist.", exception.getMessage());
+    }
+
+    @Test
+    void shouldNotThrowExceptionIfProductNotInWishlist() {
+        String clientId = "client-id";
+        Product product = Product.builder().id("product-id").build();
+        Wishlist wishlist = Wishlist.builder()
+                .clientId(clientId)
+                .products(new ArrayList<>())
+                .build();
+
+        assertDoesNotThrow(() -> wishlistValidationService.validateProductNotInWishlist(wishlist, product));
+    }
+}
