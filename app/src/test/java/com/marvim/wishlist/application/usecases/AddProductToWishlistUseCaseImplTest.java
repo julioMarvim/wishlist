33e89818a66f024fa48ed96edfe49b1add8aacce
julioMarvim <juliomarvim@yahoo.com.br>
2024-12-25 19:02:55 -0300

    test: remove tests related to the wishlist validation service

--- app/src/test/java/com/marvim/wishlist/application/usecases/AddProductToWishlistUseCaseImplTest.java
+++ app/src/test/java/com/marvim/wishlist/application/usecases/AddProductToWishlistUseCaseImplTest.java
@@ -1,23 +1,13 @@
 package com.marvim.wishlist.application.usecases;
 
-import com.marvim.wishlist.config.handler.exception.ProductAlreadyInWishlistException;
-import com.marvim.wishlist.config.handler.exception.WishlistLimitExceededException;
 import com.marvim.wishlist.domain.entity.Product;
-import com.marvim.wishlist.domain.entity.Wishlist;
-import com.marvim.wishlist.domain.entity.WishlistFactory;
 import com.marvim.wishlist.domain.ports.output.WishlistRepository;
-import com.marvim.wishlist.domain.service.WishlistValidationService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.ArrayList;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
