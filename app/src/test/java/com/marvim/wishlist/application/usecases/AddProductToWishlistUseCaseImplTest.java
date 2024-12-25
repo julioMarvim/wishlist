f993dc9e77ad0ec6fc5d4d934e24870f98bb9db2
julioMarvim <juliomarvim@yahoo.com.br>
2024-12-25 19:07:58 -0300

    remove unnecessary dependency in add product in wishlist

--- app/src/test/java/com/marvim/wishlist/application/usecases/AddProductToWishlistUseCaseImplTest.java
+++ app/src/test/java/com/marvim/wishlist/application/usecases/AddProductToWishlistUseCaseImplTest.java
@@ -9,18 +9,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class AddProductToWishlistUseCaseImplTest {
 
     @Mock
     private WishlistRepository wishlistRepository;
-
-    @Mock
-    private WishlistValidationService wishlistValidationService;
 
     @InjectMocks
     private AddProductToWishlistUseCaseImpl useCase;
