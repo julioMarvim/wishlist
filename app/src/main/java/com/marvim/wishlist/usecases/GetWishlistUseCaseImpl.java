0f431175c03795785dbdbb10c7d4ba8004136269
julioMarvim <juliomarvim@yahoo.com.br>
2024-12-28 17:22:18 -0300

    refactor: unification mappers

--- app/src/main/java/com/marvim/wishlist/usecases/GetWishlistUseCaseImpl.java
+++ app/src/main/java/com/marvim/wishlist/usecases/GetWishlistUseCaseImpl.java
@@ -3,7 +3,7 @@ package com.marvim.wishlist.usecases;
 import com.marvim.wishlist.input.GetWishlistUseCase;
 import com.marvim.wishlist.input.dto.response.WishlistResponseInput;
 import com.marvim.wishlist.output.WishlistRepository;
-import com.marvim.wishlist.usecases.mapper.WishlistToInputMapper;
+import com.marvim.wishlist.usecases.mapper.UseCaseMappers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +20,6 @@ public class GetWishlistUseCaseImpl implements GetWishlistUseCase {
     @Override
     public WishlistResponseInput execute(String clientId) {
         logger.info("Starting operation to fetch wishlist for client with ID: {}", clientId);
-        return WishlistToInputMapper.toInputDto(wishlistRepository.findOrCreate(clientId));
+        return UseCaseMappers.toInput(wishlistRepository.findOrCreate(clientId));
     }
 }
--- app/src/main/java/com/marvim/wishlist/usecases/mapper/UseCaseMappers.java
+++ app/src/main/java/com/marvim/wishlist/usecases/mapper/UseCaseMappers.java
@@ -1,21 +1,26 @@
 package com.marvim.wishlist.usecases.mapper;
 
+import com.marvim.wishlist.input.dto.request.AddProductRequestInput;
 import com.marvim.wishlist.input.dto.response.ProductResponseInput;
 import com.marvim.wishlist.input.dto.response.WishlistResponseInput;
+import com.marvim.wishlist.output.dto.request.AddProductRequestOutput;
 import com.marvim.wishlist.output.dto.response.ProductResponseOutput;
 import com.marvim.wishlist.output.dto.response.WishlistResponseOutput;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class WishlistToInputMapper {
+public class UseCaseMappers {
+    public static AddProductRequestOutput toOutput(AddProductRequestInput request) {
+        return new AddProductRequestOutput(request.id(), request.name(), request.description());
+    }
 
-    public static WishlistResponseInput toInputDto(WishlistResponseOutput wishlistResponseOutput) {
-        List<ProductResponseInput> productResponses = toProductsInputDto(wishlistResponseOutput.products());
+    public static WishlistResponseInput toInput(WishlistResponseOutput wishlistResponseOutput) {
+        List<ProductResponseInput> productResponses = toProducts(wishlistResponseOutput.products());
         return new WishlistResponseInput(wishlistResponseOutput.id(), wishlistResponseOutput.clientId(), productResponses);
     }
 
-    private static List<ProductResponseInput> toProductsInputDto(List<ProductResponseOutput> productResponseOutput) {
+    private static List<ProductResponseInput> toProducts(List<ProductResponseOutput> productResponseOutput) {
         return productResponseOutput.stream()
                 .map(product -> new ProductResponseInput(
                         product.id(),
